06cfc7e775a6a3372b2df8a14668157245f8a2e0
bn21293 <74336195+bn21293@users.noreply.github.com>
2022-10-07 11:59:27 +0900

    Update vqt.m

--- vqt.m
+++ vqt.m
@@ -1,7 +1,7 @@
 clear all
 close all
 clc
-cd('C:\Users\kk260\Desktop\ProjectItou2020\Script')% folder path
+cd('C:\Users\')% folder path
 load('vq.mat')% filename
 vq = fliplr(rot90(rot90(vq)));
 imagesc(log(abs(vq)))
